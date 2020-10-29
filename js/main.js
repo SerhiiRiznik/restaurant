589aab8212ce33857b3220d7d985cffcbbf5c7d5
SerhiiRiznik <69801777+SerhiiRiznik@users.noreply.github.com>
2020-10-29 18:53:25 +0100

    add test slide

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('hi serg')
+
+$(document).ready(function () {
+   $('.slider').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+
+   });
+});
