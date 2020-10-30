93b5d26cd0ca2b7157436be99901b28a19f2b0fb
SerhiiRiznik <69801777+SerhiiRiznik@users.noreply.github.com>
2020-10-30 14:28:02 +0100

    version 2.0

--- js/main.js
+++ js/main.js
@@ -1,11 +1,46 @@
 
 $(document).ready(function () {
+
    $('.slider').slick({
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000,
+      centerMode: true,
+      variableWidth: true,
+      responsive: [
 
+         {
+            breakpoint: 500,
+            settings: {
+
+               slidesToShow: 1,
+               slidesToScroll: 1,
+            }
+         }
+      ]
    });
 });
+
+
+// slider
+// $(document).ready(function () {
+
+//    $('.slider').slick({
+//       infinite: true,
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       autoplay: true,
+//       autoplaySpeed: 2000,
+//       responsive: [
+//          {
+//             breakpoint: 600,
+//             settings: {
+//                slidesToShow: 1,
+//                slidesToScroll: 1
+//             }
+//          }
+//       ]
+//    });
+// });
